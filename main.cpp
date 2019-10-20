89c1eff6027422512be1226d452057673e573b03
limengfan.wlj <limengfan.wlj@bytedance.com>
2019-10-20 20:27:18 +0800

    init opengl with some libs

--- main.cpp
+++ main.cpp
@@ -1,3 +1,13 @@
+#include <glad.h>
+#include <glfw3.h>
+#include <glm.hpp>
+#include <gtc/matrix_transform.hpp>
+#include <gtc/type_ptr.hpp>
+#include <Importer.hpp>
+#include <scene.h>
+#include <postprocess.h>
+#include <ft2build.h>
+#include <stb_image.h>
 #include <iostream>
 
 int main() {
