6db036b90b830b247bee05d6ecd3eaddbedfaa56
limengfan.wlj <limengfan.wlj@bytedance.com>
2019-10-20 21:39:27 +0800

    new utils

--- main.cpp
+++ main.cpp
@@ -1,16 +1,106 @@
 #include <glad.h>
 #include <glfw3.h>
-#include <glm.hpp>
-#include <gtc/matrix_transform.hpp>
-#include <gtc/type_ptr.hpp>
-#include <Importer.hpp>
-#include <scene.h>
-#include <postprocess.h>
-#include <ft2build.h>
-#include <stb_image.h>
+
+#include "game.h"
+#include "resource_manager.h"
+
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+void key_callback(GLFWwindow *window, int key, int scancode, int action, int mode);
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height);
+
+// The Width of the screen
+const GLuint SCREEN_WIDTH = 800;
+// The height of the screen
+const GLuint SCREEN_HEIGHT = 600;
+
+Game Breakout(SCREEN_WIDTH, SCREEN_HEIGHT);
+
+int main(int argc, char *argv[]) {
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // uncomment this statement to fix compilation on OS X
+#endif
+
+    GLFWwindow *window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Breakout", nullptr, nullptr);
+    if (window == nullptr) {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetKeyCallback(window, key_callback);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    // OpenGL configuration
+    glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+    glEnable(GL_CULL_FACE);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+    // Initialize game
+    Breakout.Init();
+
+    // DeltaTime variables
+    GLfloat deltaTime = 0.0f;
+    GLfloat lastFrame = 0.0f;
+
+    // Start Game within Menu State
+    Breakout.State = GAME_ACTIVE;
+
+    while (!glfwWindowShouldClose(window)) {
+        // Calculate delta time
+        GLfloat currentFrame = glfwGetTime();
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
+        glfwPollEvents();
+
+        //deltaTime = 0.001f;
+        // Manage user input
+        Breakout.ProcessInput(deltaTime);
+
+        // Update Game state
+        Breakout.Update(deltaTime);
+
+        // Render
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        Breakout.Render();
+
+        glfwSwapBuffers(window);
+    }
+
+    // Delete all resources as loaded using the resource manager
+    ResourceManager::Clear();
+
+    glfwTerminate();
     return 0;
+}
+
+void key_callback(GLFWwindow *window, int key, int scancode, int action, int mode) {
+    // When a user presses the escape key, we set the WindowShouldClose property to true, closing the application
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, GL_TRUE);
+    }
+    if (key >= 0 && key < 1024) {
+        if (action == GLFW_PRESS) {
+            Breakout.Keys[key] = GL_TRUE;
+        } else if (action == GLFW_RELEASE) {
+            Breakout.Keys[key] = GL_FALSE;
+        }
+    }
+}
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
+    glViewport(0, 0, width, height);
 }
