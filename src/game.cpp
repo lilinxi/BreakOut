04ae194ee5c7f5dc863a8018afaea171fc1968aa
limengfan.wlj <limengfan.wlj@bytedance.com>
2019-10-21 20:29:26 +0800

    fix bug

--- src/game.cpp
+++ src/game.cpp
@@ -86,11 +86,17 @@ void Game::ProcessInput(GLfloat dt) {
         if (this->Keys[GLFW_KEY_A]) {
             if (Player->Position.x >= 0) {
                 Player->Position.x -= velocity;
+                if (Ball->Stuck) {
+                    Ball->Position.x -= velocity;
+                }
             }
         }
         if (this->Keys[GLFW_KEY_D]) {
             if (Player->Position.x <= this->Width - Player->Size.x) {
                 Player->Position.x += velocity;
+                if (Ball->Stuck) {
+                    Ball->Position.x += velocity;
+                }
             }
         }
         // 释放球
