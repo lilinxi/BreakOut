16acbb23c5d863fd8e3505e15497e6e4444b2167
limengfan.wlj <limengfan.wlj@bytedance.com>
2019-10-21 16:52:01 +0800

    finish ball

--- src/game.h
+++ src/game.h
@@ -24,6 +24,11 @@ const glm::vec2 PLAYER_SIZE(100, 20);
 // 初始化当班的速率
 const GLfloat PLAYER_VELOCITY(500.0f);
 
+// 初始化球的速度
+const glm::vec2 INITIAL_BALL_VELOCITY(100.0f, -350.0f);
+// 球的半径
+const GLfloat BALL_RADIUS = 12.5f;
+
 // Game holds all game-related state and functionality.
 // Combines all game-related data into a single class for
 // easy access to each of the components and manageability.
